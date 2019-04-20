119933b238f2aec4691c7c0b468dda76e4e0889c
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-04-20 11:50:44 -0700

    created superuser

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recommend.apps.RecommendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
